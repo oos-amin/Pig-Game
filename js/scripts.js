4fb1dd1cd34081f3468dd42754dbe702f5ed6d46
Amin Moeini <amin47moeini@gmail.com>
2025-11-26 06:26:49 +0330

    script: add dom elements

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Elements
+const player0El = document.querySelector("#player__name-0");
+const player1El = document.querySelector("#player__name-1");
+const playerName0El = document.querySelector("#player__name-0");
+const playerName1El = document.querySelector("#player__name-1");
+const diceImageEl = document.querySelector(".dice-image");
+const currentPlayer0El = document.querySelector("#player-current-0");
+const currentPlayer1El = document.querySelector("#player-current-1");
+const btnNew = document.querySelector(".btn__new");
+const btnDice = document.querySelector(".btn__dice");
+const btnHold = document.querySelector(".btn__hold");
